cbc03fc0de08f3df0ee5e0f9e4977fffadb8e008
Sergey Chernikov <Sergey.Chernikov@havi.com>
2023-08-09 12:50:25 +0600

    remove useless  scripts

--- src/BE/grocery_supermarket_api/entity/api_objects.py
+++
@@ -1,8 +0,0 @@
-import json
-class Response:
-    def __init__(self, status:str = None, message:str = None, data:str = None):
-        self.status = status
-        self.message = message
-        self.data = data
-    def __json__(self):
-        return json.dumps(self.__dict__, indent=2)
